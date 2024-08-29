0846f451b6433c2b9acec350dd8f1a911ee0e37f
zhangyafei <1187254196@qq.com>
2024-08-29 15:39:01 +0800

    feat: 处理端口号占用时,自动开新端口

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,12 +2,13 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const base = require('./webpack.base.js');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const portfinder = require('portfinder');
 
-module.exports = merge(base, {
+const devConfig = merge(base, {
   mode: 'development', // development:开发环境,内存打包  production:生产环境，硬盘打包
   devtool: 'eval-cheap-module-source-map', // 生成map文件，方便调试
   devServer: {
-    open: false, // 自动打开浏览器
+    open: true, // 自动打开浏览器
     port: 3344,
     hot: true, // 热更新
     historyApiFallback: true, // 解决history路由404问题
@@ -33,4 +34,19 @@ module.exports = merge(base, {
   plugins: [
     new ReactRefreshWebpackPlugin(), // react热更新
   ]
+});
+
+// 如果端口占用，开新端口
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = devConfig.devServer.port;
+  // 从指定的端口开始查找空闲端口
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    // 端口占用重设新端口
+    devConfig.devServer.port = process.env.PORT = port;
+    resolve(devConfig);
+  });
 });
