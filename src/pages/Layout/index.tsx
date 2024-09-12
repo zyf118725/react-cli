4f69b8b07910631977f5cc23e5b3dbbe49894ab1
zhangyafei <1187254196@qq.com>
2024-09-12 18:05:04 +0800

    feat: 优化layout

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from "react-router-dom";
-import { PageContainer, ProCard, ProLayout } from '@ant-design/pro-components';
+import { ProLayout } from '@ant-design/pro-components';
 import useRoute from '@/hooks/useRoute';
-// import Img from '@/utils/img'
 import { menuList } from '@/routes';
 import MyHeader from './Header';
+
 
 export default function Aside() {
   const { goto, getPageParams } = useRoute();
   const [pathname, setPathname] = useState('');
-
   const defaultProps = {
     route: {
       path: '/',
@@ -22,55 +21,33 @@ export default function Aside() {
     setPathname(window.location.pathname);
   }, []);
 
+  const menuStyle = {
+    sider: {
+      colorMenuBackground: '#fff',
+      colorMenuItemDivider: '#dfdfdf',
+      colorTextMenu: '#595959',
+      colorTextMenuSelected: 'rgba(42,122,251,1)',
+      colorBgMenuItemSelected: 'rgba(230,243,254,1)',
+    }
+  };
+
   return (
-    <div
-      id="test-pro-layout"
-      style={{ height: '100vh' }}
-    >
+    <div id="test-pro-layout" style={{ height: '100vh' }} >
       <ProLayout
         title='权限引擎'
         // logo={Img.logo}
         siderWidth={216}
+        fixSiderbar={true}
         {...defaultProps}
-        // header={() => (<p>header</p>)}
-        headerRender={() => (<p>header</p>)}
-        headerContentRender={() => (<p>header</p>)}
-        token={{
-          sider: {
-            colorMenuBackground: '#fff',
-            colorMenuItemDivider: '#dfdfdf',
-            colorTextMenu: '#595959',
-            colorTextMenuSelected: 'rgba(42,122,251,1)',
-            colorBgMenuItemSelected: 'rgba(230,243,254,1)',
-          }
-        }}
-        menuItemRender={(item, dom) => (
-          <div
-            onClick={() => {
-              setPathname(item.path || '/welcome');
-              goto(item.path || '/');
-            }}
-          >
-            {dom}
-          </div>
-        )}
+        token={menuStyle}
+        // 自动展开菜单项
+        menu={{ defaultOpenAll: true, autoClose: false }}
+        menuItemRender={(item, dom) => <div onClick={() => { setPathname(item.path || '/'); goto(item.path || '/'); }}>{dom}</div>}
         // 头部logo的点击事件
         onMenuHeaderClick={(e) => goto('/')}
-      // breadcrumbRender={false} // 隐藏面包屑
       >
         <MyHeader />
         <Outlet />
-
-        {/* <PageContainer>
-          <ProCard
-            style={{
-              height: '100vh',
-              minHeight: 800,
-            }}
-          >
-            <Outlet />
-          </ProCard>
-        </PageContainer> */}
       </ProLayout>
     </div >
   );
