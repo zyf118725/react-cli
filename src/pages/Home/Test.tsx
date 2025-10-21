9cb24715b50d0c32dc4d4f391f4a234d958a5deb
zhangyafei <1187254196@qq.com>
2025-10-21 14:36:44 +0800

    feat: 清理冗余代码

--- src/pages/Home/Test.tsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <>
-      <div className="my-[4px] bg-gray-500 w-[180px] text-green-400">tailWind css</div>
-    </>
-  );
-}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,3 @@
-import { useEffect } from 'react';
-import { goodsList } from '@/api';
-import { Button, message } from 'antd';
-
 export default function Home() {
-  useEffect(() => {
-    getData();
-  }, []);
-
-  // 获取数据
-  const getData = async () => {
-    const res: any = await goodsList({});
-    console.log('res: ', res);
-  };
-
-  return (
-    <div>
-      Home
-      <Button onClick={() => message.success('成功')}>msg</Button>
-    </div>
-  );
+  return <div>Home</div>;
 }
